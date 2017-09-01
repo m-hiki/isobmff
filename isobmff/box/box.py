b74675f34d352e567c41f63be6cc2eb1257d5786
Minoru Hiki <minoruhiki@gmail.com>
2017-09-01 17:07:17 +0900

    BoxMetaでフィールドを読むためのpropertyを定義する実装を追加。ftypをBoxの仕様に合うように修正

--- isobmff/box/box.py
+++ isobmff/box/box.py
@@ -1,27 +1,49 @@
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
 from enum import Enum
-import re
 from .field import Field, Int, String
+from io import BytesIO
+import re
+
+
+def gen_property(name, klass):
+    storage_name = '_' + name
+    
+    @property
+    def prop(self):
+        print(self)
+        return getattr(self, storage_name).value
+    
+    @prop.setter
+    def prop(self, value):
+        getattr(self, storage_name).value = value
+
+    return prop
 
 
 class BoxMeta(type):
     box_list = {}
     
     @classmethod
-    def __prepare__(mcs, name, bases, *, box_type):
+    def __prepare__(mcs, name, bases, *, box_type, extended_type=None):
         return OrderedDict()
 
-    def __new__(mcs, clsname, bases, clsdict, *, box_type):
-        clsdict['box_type'] = box_type
-        cls = type.__new__(mcs, clsname, bases, clsdict)        
+    def __new__(mcs, clsname, bases, clsdict, *, box_type, extended_type=None):
+        #clsdict['box_type'] = box_type        
+        cls = type.__new__(mcs, clsname, bases, dict(clsdict))
         if box_type:
             mcs.box_list[box_type] = cls
+        
         fields = ((k, v) for k, v in clsdict.items() if isinstance(v, Field))
         cls.fields = OrderedDict(fields)
+
+        # Field object is renamed and replaced to property
+        for name, field in cls.fields.items():
+            setattr(cls, '_' + name, field)
+            setattr(cls, name, gen_property(name, field))
         return cls
 
-    def __init__(self, clsname, bases, clsdict, *, box_type):
+    def __init__(self, clsname, bases, clsdict, *, box_type, extended_type=None):
         super().__init__(clsname, bases, clsdict)
 
 
@@ -29,45 +51,28 @@ class Box(metaclass=BoxMeta, box_type=None):
     size = Int(32)
     typ = String(32)
 
-    @classmethod
-    def get_class_tree(cls, target):
-        tree = []
-        while target != cls:
-            tree.insert(0, target)
-            target = target.__base__
-        return tree
-
     def get_box_size(self):
         """get box size excluding header"""
         return self.size - 8
 
     def read_field(self, file):
-        for name, field in self.fields.items():            
+        for name, field in self.fields.items():
             field.read(file)
-            print(name + ' ' + str(field.value))
+            #print(name + ' ' + str(field))
         
     def read(self, file):
         self.read_field(file)
-        tree = Box.get_class_tree(BoxMeta.box_list[self.typ.value])
-        for cls in tree:
+        box_size = self.size - (self._size.size + self._typ.size)
+        buff = BytesIO(file.read(box_size))
+        #print(self.typ + '(' + str(self.size) + ')')
+        for cls in get_class_tree(Box, BoxMeta.box_list[self.typ]):
             self.__class__ = cls
-            self.read_field(file)
+            self.read_field(buff)
 
     def write(self, file):
         """write box to file"""
         pass
 
-class Container(object):
-    def read(self, file):
-        read_size = self.get_box_size()
-        #print(file.read(read_size))
-        while read_size > 0:
-            box = read_box(file)
-            if not box:
-                break
-            #TODO: Quantityでそのままsetattrか配列にappendか分ける
-            setattr(self, box.box_type, box)
-            read_size -= box.size
 
 class Quantity(Enum):
     ZERO_OR_ONE = 0
@@ -75,5 +80,29 @@ class Quantity(Enum):
     ONE_OR_MORE = 2
     ANY_NUMBER = 3
 
+
+class BoxContainer(Field):
+    def read(self, file):
+        self.value = []
+        read_size = self.size
+
+        while read_size > 0:
+            box = Box()
+            box.read(file)
+            #TODO: Quantityでそのままsetattrか配列にappendか分ける
+            if not box.size:
+                break
+            self.value.append(box)
+            read_size -= box.size
+
 def indent(rep):
     return re.sub(r'^', '  ', rep, flags=re.M)
+
+
+def get_class_tree(cls, target):
+    tree = []
+    while target != cls:
+        tree.insert(0, target)
+        target = target.__base__
+    return tree
+
--- isobmff/box/ftyp.py
+++ isobmff/box/ftyp.py
@@ -1,30 +1,11 @@
 # -*- coding: utf-8 -*-
-from .box import Box, Quantity, read_box, read_int, read_string
+from .box import Box, Quantity
+from .field import Int, String, ListToBoxEnd
 
 
 class FileTypeBox(Box, box_type='ftyp'):
     is_mandatory = True
     quantity = Quantity.EXACTLY_ONE
-
-
-    def __init__(self, size):
-        super().__init__(size=size)
-        self.majar_brand = None
-        self.minor_version = None
-        self.compatible_brands = []
-
-    def __repr__(self):
-        rep = 'majar_brand: ' + self.majar_brand + '\n'
-        rep += 'minor_version: ' + str(self.minor_version) + '\n'
-        rep += 'compatible_brands: '
-        for brand in self.compatible_brands:
-            rep += brand + ','
-        return super().__repr__() + indent(rep)
-
-    def read(self, file):
-        self.majar_brand = read_string(file, 4)
-        self.minor_version = read_int(file, 4)
-        num_compatible_brands = int((self.size - 16) / 4)
-        for _ in range(num_compatible_brands):
-            compat_brand = read_string(file, 4)
-            self.compatible_brands.append(compat_brand)
+    majar_brand = String(32)
+    minor_version = String(32)
+    compatible_brands = ListToBoxEnd(String(32))
